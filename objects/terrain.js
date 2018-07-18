b89e57f6bc1ba73172a5b1c259462a1d0a1fc027
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-07-18 14:17:35 -0400

    Update terrain.js

--- objects/terrain.js
+++ objects/terrain.js
@@ -1,1 +1,8 @@
+var boundaries = [];
 
+function terrain() {
+    for (blocksPlaced = 0; blocksPlaced < width; blocksPlaced++) {
+        noiseVal = ((noise(blocksPlaced*0.0007))*height/21);
+        boundaries.push(new Boundary(blocksPlaced, noiseVal*27+600, 2, 1000, 0))
+    }
+}
