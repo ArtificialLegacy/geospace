3ef251cfcccda5f9ef563785623a37b337641858
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-07-18 14:16:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
+var engine;
+var world;
+var boxes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  terrain();
+}
+
+function mousePressed() {
+  boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));
+}
+
+function draw() {
+    clear();
+    fill(51, 204, 255);
+    rect(0, 0, width, height);
+    Engine.update(engine);
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].show();
+    }
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].show();
+    }
+}
